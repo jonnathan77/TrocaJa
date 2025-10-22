e55505f3764bd259e95f482dc768bc25a74ca554
Jonnathan Cruz <jonnathanfsouzacruz@gmail.com>
2025-10-21 22:54:08 -0300

    ajustando variaveis ambiente

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,12 +1,20 @@
-// firebase.js
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
+require('dotenv').config();
 
-// ⚠️ Baixe o arquivo JSON da sua conta de serviço pelo Console do Firebase
-// (Configurações do projeto > Contas de serviço > Gerar chave privada)
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
