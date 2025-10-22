1d50f460e72552599dfe14a202a3e11263bacea3
Jonnathan Cruz <jonnathanfsouzacruz@gmail.com>
2025-10-21 22:28:03 -0300

    ajustando variaveis ambiente

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 
 // ⚠️ Baixe o arquivo JSON da sua conta de serviço pelo Console do Firebase
 // (Configurações do projeto > Contas de serviço > Gerar chave privada)
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
